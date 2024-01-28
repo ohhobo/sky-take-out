beaf8f04bfd550015af62c8dd3853e89a7b5a38d
ohhobo <10819603+ohhobo@user.noreply.gitee.com>
2024-01-28 18:03:34 +0800

    下单提醒和用户催单

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,54 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: cyr
+ * @Date: 2024/01/27/18:11
+ * @Description:
+ */
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void executeTimeOutOrder(){
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.PENDING_PAYMENT, time);
+        if(ordersList!=null||ordersList.size()>0){
+            ordersList.forEach(orders -> {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelTime(LocalDateTime.now());
+                orders.setCancelReason("支付超时，取消交易");
+                orderMapper.update(orders);
+            });
+        }
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void executeDeliverOrder(){
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrdertimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+
+        if(ordersList != null && ordersList.size() > 0){
+            ordersList.forEach(order -> {
+                order.setStatus(Orders.COMPLETED);
+                orderMapper.update(order);
+            });
+        }
+    }
+
+}
